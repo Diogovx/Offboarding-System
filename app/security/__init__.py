c64483b05257a3084068f800c634ef3a8c465f33
Diogo Velozo Xavier <diogo.velozo@outlook.com>
2025-11-24 11:12:54 -0300

    :bricks: ci: Added the first migration with Alembic. Created user table.

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -6,6 +6,7 @@ from .security import (
     create_access_token
 )
 from .deps import get_current_user, require_admin, require_editor
+from .settings import Settings
 
 __all__ = [
     "get_password_hash",
@@ -15,5 +16,6 @@ __all__ = [
     "ALGORITHM",
     "get_current_user",
     "require_admin",
-    "require_editor"
+    "require_editor",
+    "Settings"
 ]
