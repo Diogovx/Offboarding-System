d3375214bf9d3803dad0a339568a92d0221cc740
Lucas Matias <lucasmatias2005@gmail.com>
2026-01-08 14:52:29 -0300

    ✨ Adding the orchestrator API route to Main! (execute)

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.database import init_db
-from app.routers import aduser_router, auth_router, system_router, user_router, intouch_router
-
+from app.routers import aduser_router, auth_router, execute_router, system_router, user_router, intouch_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,3 +19,4 @@ app.include_router(auth_router.router)
 app.include_router(user_router.router)
 app.include_router(aduser_router.router)
 app.include_router(intouch_router.router)
+app.include_router(execute_router.router)
