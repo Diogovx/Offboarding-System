66279524048fca55aac9df792dce11fd8a9bb1ed
Lucas Matias <lucasmatias2005@gmail.com>
2026-01-09 11:12:03 -0300

    ✨ feature: Adding logic to disable the AD user in the EXECUTE service

--- app/services/execute_service.py
+++ app/services/execute_service.py
@@ -1,12 +1,22 @@
+from app.models.ad_user_model import DisableUserRequest
+from app.routers import aduser_router
 from app.services import intouch_service
 from app.services.email_service import send_email
 from app.enums import EmailActions
 
 async def execute_offboarding(matricula, current_user, background_tasks):
-    resultado = intouch_service.desativar_funcionario(matricula)
+
+    payload_ad = DisableUserRequest(
+        registration=matricula,
+        performed_by=current_user.username 
+    )
+
+    res_intouch =  await intouch_service.desativar_funcionario(matricula)
+    res_ad = await aduser_router.disable_user(payload=payload_ad, session=current_user)
     
-    if not resultado.get("success"):
-        return {"success": False, "error": resultado.get("error")}
+    if not res_intouch.get("success") or not res_ad.get("sucess"):
+        error_msg = f"InTouch: {res_intouch.get('error')} | AD: {res_ad.get('error')}"
+        return {"success": False, "error": error_msg}
 
    
     action = EmailActions.get_by_id(3)
@@ -15,6 +25,10 @@ async def execute_offboarding(matricula, current_user, background_tasks):
   
     return {
         "success": True,
-        "message": resultado.get("message"),
-        "acao": resultado.get("acao")
+        "message": res_intouch.get("message"),
+        "acao": res_intouch.get("acao"),
+        "details": {
+            "intouch": res_intouch.get("message"),
+            "ad": res_ad.get("message")
+        }
     }
