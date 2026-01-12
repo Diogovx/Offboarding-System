10fffbbfd8e40a8085b6bfeeff7206d2f4343f2b
Lucas Matias <lucasmatias2005@gmail.com>
2026-01-12 14:15:55 -0300

    ✨ feat: Adding a list as a parameter to store disabled services

--- app/services/execute_service.py
+++ app/services/execute_service.py
@@ -5,30 +5,37 @@ from app.services.email_service import send_email
 from app.enums import EmailActions
 
 async def execute_offboarding(registration, current_user, background_tasks):
+    services_list = []
 
-    payload_ad = DisableUserRequest(
-        registration=registration,
-        performed_by=current_user.username 
-    )
+    res_intouch = await intouch_service.deactivate_user_intouch(registration)
+    if res_intouch.get("success"):
+        services_list.append("Intouch")
 
-    res_intouch =  await intouch_service.deactivate_user_intouch(registration)
-    res_ad = await aduser_router.disable_user(payload=payload_ad, session=current_user)
-    
-    if not res_intouch.get("success") or not res_ad.get("success"):
-        error_msg = f"InTouch: {res_intouch.get('error')} | AD: {res_ad.get('error')}"
-        return {"success": False, "error": error_msg}
+    try:
+        payload_ad = DisableUserRequest(registration=registration, performed_by=current_user.username)
+        res_ad = await aduser_router.disable_user(payload=payload_ad, session=current_user)
+        
+        if res_ad.get("success"):
+            services_list.append("Active Directory")
+        else:
+            print(f"AD returned success=False: {res_ad.get('message')}")
+    except Exception as e:
 
-   
-    action = EmailActions.get_by_id(3)
-    background_tasks.add_task(send_email, registration, action)
+        print(f"AD skip: {e}")
 
   
+    action = EmailActions.get_by_id(3)
+    
+
+    background_tasks.add_task(
+        send_email, 
+        registration=registration, 
+        action=action, 
+        performed_by=str(current_user.username), 
+        systems_list=services_list             
+    )
+
     return {
         "success": True,
-        "message": res_intouch.get("message"),
-        "acao": res_intouch.get("acao"),
-        "details": {
-            "intouch": res_intouch.get("message"),
-            "ad": res_ad.get("message")
-        }
+        "details": services_list 
     }
