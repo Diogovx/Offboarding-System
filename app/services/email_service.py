5b31317bfefd82406efbd3f0956726f0e047d2bb
Lucas Matias <lucasmatias2005@gmail.com>
2026-01-12 14:13:40 -0300

    ✨ feat: Adding the disabled services and those responsible for the action to the email message

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -8,39 +8,31 @@ from app.enums import EmailActions
 
 settings = Settings()
 
-async def send_email(registration: str, action: EmailActions):
 
+async def send_email(registration: str, action: EmailActions, performed_by: str = "System", systems_list: list = None):
     now = datetime.now(ZoneInfo("America/Sao_Paulo")).strftime("%d/%m/%Y %H:%M:%S")
-    sender_email = settings.EMAIL_SENDER
-    password = settings.EMAIL_PASSWORD
-    receiver_email = settings.EMAIL_RECEIVER
+    
+    if systems_list and len(systems_list) > 0:
+        items = "\n".join([f"- {s}" for s in systems_list])
+        txt_details = f"\nSystems processed successfully:\n{items}"
+    else:
+      
+        txt_details = "\nWarning: No systems were successfully updated in this task."
 
-
-    if not settings.SMTP_SERVER:
-        raise RuntimeError("EMAIL_SMTP_HOST not configured")
-
- 
-    if not sender_email or not password or not receiver_email:
-        raise RuntimeError("Email variables not configured")
-
- 
     msg = EmailMessage()
-   
-    msg["Subject"] = f"User {action.value.upper()} - Registration {registration}"
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
+    msg["Subject"] = f"Offboarding Log - {registration}"
+    msg["From"] = settings.EMAIL_SENDER
+    msg["To"] = settings.EMAIL_RECEIVER
+
     msg.set_content(
-        f"The user with registration {registration} was successfully {action.value} in the system.\n"
+        f"The user with registration {registration} was processed ({action.value}).\n"
+        f"Executor: {performed_by}\n" 
+        f"{txt_details}\n"
         f"Date/Time: {now}"
     )
 
-
     context = ssl.create_default_context()
-
-   
-    with smtplib.SMTP(
-        settings.SMTP_SERVER, settings.PORT, timeout=10
-    ) as server:
+    with smtplib.SMTP(settings.SMTP_SERVER, settings.PORT, timeout=10) as server:
         server.starttls(context=context) 
-        server.login(sender_email, password)
+        server.login(settings.EMAIL_SENDER, settings.EMAIL_PASSWORD)
         server.send_message(msg)
