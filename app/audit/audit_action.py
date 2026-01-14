44f3ca5dbdc69e736172314e3dce6f628ba6bcbe
Diogo Velozo Xavier <diogo.velozo@outlook.com>
2026-01-14 11:52:52 -0300

    :recycle: refactor: Values ​​changed to lowercas

--- app/audit/audit_action.py
+++ app/audit/audit_action.py
@@ -2,21 +2,21 @@ from enum import Enum
 
 
 class AuditAction(str, Enum):
-    SYSTEM_LOGIN = "SYSTEM_LOGIN"
-    SYSTEM_LOGOUT = "SYSTEM_LOGOUT"
-    TOKEN_REFRESH = "TOKEN_REFRESH"
+    SYSTEM_LOGIN = "system_login"
+    SYSTEM_LOGOUT = "system_logout"
+    TOKEN_REFRESH = "token_refresh"
 
-    LIST_USERS = "LIST_USERS"
-    CREATE_USER = "CREATE_USER"
-    UPDATE_USER = "UPDATE_USER"
-    READ_CURRENT_USER = "READ_CURRENT_USER"
+    LIST_USERS = "list_users"
+    CREATE_USER = "create_user"
+    UPDATE_USER = "update_user"
+    READ_CURRENT_USER = "read_current_user"
 
-    SEARCH_AD_USER = "SEARCH_AD_USER"
-    DISABLE_AD_USER = "DISABLE_AD_USER"
+    SEARCH_AD_USER = "search_ad_user"
+    DISABLE_AD_USER = "disable_ad_user"
 
-    VIEW_AUDIT_LOGS = "VIEW_AUDIT_LOGS"
-    EXPORT_AUDIT_LOGS = "EXPORT_AUDIT_LOGS"
+    VIEW_AUDIT_LOGS = "view_audit_logs"
+    EXPORT_AUDIT_LOGS = "export_audit_logs"
 
-    SYSTEM_START = "SYSTEM_START"
-    SYSTEM_STOP = "SYSTEM_STOP"
-    SYSTEM_ERROR = "SYSTEM_ERROR"
+    SYSTEM_START = "system_start"
+    SYSTEM_STOP = "system_stop"
+    SYSTEM_ERROR = "system_error"
