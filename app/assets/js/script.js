2edc78e4a6bf0f60e0774205ac85f93e0cdc37b2
Lucas Matias <lucasmatias2005@gmail.com>
2026-01-26 14:44:48 -0300

    ✨ feat: configuring CORS to connect to the back-end

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,22 +1,39 @@
-const { createApp, ref } = Vue
+const { createApp, ref } = Vue;
 
 createApp({
     setup() {
- 
-        const title_page = ref('Connect your account')
-        const email = ref('')
-        const password = ref('')
+        const title_page = ref('Connect your account');
+        const username = ref(''); 
+        const password = ref('');
 
-        function signup() {
-            console.log('Login in progress:', email.value, password.value)
-            alert('Login submitted! Check the console.')
+       
+        async function signup() {
+            console.log("Trying to log in with:", username.value);
+            
+
+            const url = "http://127.0.0.1:8000/token";
+            
+            const params = new URLSearchParams();
+            params.append('username', username.value); 
+            params.append('password', password.value);
+
+            try {
+                const response = await axios.post(url, params);
+                localStorage.setItem("access_token", response.data.access_token);
+                alert("Success!");
+                window.location.href = "/dashboard"; 
+            } catch (error) {
+                const msg = error.response?.data?.detail || "Connection error";
+                alert("Login failed: " + msg);
+            }
         }
 
+       
         return {
             title_page,
-            email,
+            username,
             password,
             signup
-        }
+        };
     }
-}).mount('#app')
+}).mount('#app');
