acab73f58439367ca095cedcecc6a40171b120a9
Lucas Matias <lucasmatias2005@gmail.com>
2026-01-27 15:11:59 -0300

    ✨ feat: Receiving the user in the forms (localStorage)

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -2,33 +2,45 @@ const { createApp, ref } = Vue;
 
 createApp({
     setup() {
-        const title_page = ref('Connect your account');
+ 
+        const title_page = ref('access your account');
         const username = ref(''); 
         const password = ref('');
 
-       
-        async function signup() {
+        
+        const signup = async () => {
             console.log("Trying to log in with:", username.value);
-            
 
-            const url = "http://127.0.0.1:8000/token";
+            
+            const url = "http://127.0.0.1:8000/token"; 
             
             const params = new URLSearchParams();
             params.append('username', username.value); 
             params.append('password', password.value);
+try {
+    const response = await axios.post(url, params);
+    
+    
+    if (response.data.access_token) {
+        localStorage.setItem("access_token", response.data.access_token);
+        localStorage.setItem("username", username.value);
 
-            try {
-                const response = await axios.post(url, params);
-                localStorage.setItem("access_token", response.data.access_token);
-                alert("Success!");
-                window.location.href = "/dashboard"; 
-            } catch (error) {
-                const msg = error.response?.data?.detail || "Connection error";
-                alert("Login failed: " + msg);
-            }
-        }
+        console.log("Login OK, Redirecting...");
+        
+  
+        window.location.href = "offboarding.html"; 
+    } else {
+        alert("Error: Token not received from server.");
+    }
 
-       
+} catch (error) {
+  
+    console.error(error);
+    alert("Login failed!");
+}
+        };
+
+    
         return {
             title_page,
             username,
