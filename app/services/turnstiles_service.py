081eb7e17d004fc76beb30577e6869aa1e9bb029
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-03 15:08:23 -0300

    ✨ feat: Adding turnstile service

---
+++ app/services/turnstiles_service.py
@@ -0,0 +1,36 @@
+import requests
+from http import HTTPStatus
+from app.security import settings
+
+TURNSTILES = [
+    {"name": "Unit A", "url": settings.TURNSTILE_A_URL, "session": settings.TURNSTILE_A_SESSION},
+    {"name": "Unit B", "url": settings.TURNSTILE_B_URL, "session": settings.TURNSTILE_B_SESSION}
+]
+
+async def deactivate_user_turnstiles(registration: str):
+    print(f"DEBUG: Recebi a matricula: {registration}")
+    
+    user_id = int(registration)
+    
+    payload = {
+        "object": "users",
+        "values": {
+            "begin_time": 0,
+            "end_time": 1717690651 #
+        },
+        "where": {
+            "users": { "id": user_id }
+        }
+    }
+
+    for turnstile in TURNSTILES:
+        url = f"{turnstile['url']}/modify_objects.fcgi?session={turnstile['session']}"
+        try:
+            requests.post(url, json=payload, timeout=5)
+        except Exception:
+            continue 
+
+    return {
+        "success": True,
+        "action": "turnstile_deactivation"
+    }
