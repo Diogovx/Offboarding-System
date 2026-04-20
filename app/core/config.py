a8678bef1db6ec8e7db67fd8c7c8e33158ea08d7
Diogo Velozo Xavier <diogo.velozo@outlook.com>
2026-04-20 10:11:16 -0300

    :recycle: refactor: Reorganizing the core folder to centralize settings.

---
+++ app/core/config.py
@@ -0,0 +1,44 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    model_config = SettingsConfigDict(
+        secrets_dir="/run/secrets",
+        env_file=".env", env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+    DATABASE_URL: str = ""
+    SECRET_KEY: str = ""
+    ALGORITHM: str = ""
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    INTOUCH_TOKEN: str = ""
+    INTOUCH_URL: str = ""
+    INTOUCH_API_KEY: str = "your_api_key_here"
+    EMAIL_SENDER: str = ""
+    EMAIL_PASSWORD: str = ""
+    EMAIL_RECEIVER: str = ""
+    SMTP_SERVER: str = ""
+    AD_PORT: int = 587
+    AD_USERNAME: str = "username_here"
+    AD_PASSWORD: str = "password_here"
+    AD_SERVER: str = ""
+    AD_DOMAIN: str = ""
+    AD_BASE_DN: str = ""
+    DISABLED_OU: str = ""
+    TURNSTILE_A_URL: str = ""
+    TURNSTILE_A_SESSION: str = ""
+    TURNSTILE_B_URL: str = ""
+    TURNSTILE_B_SESSION: str = ""
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
---
+++ app/core/database.py
@@ -0,0 +1,29 @@
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Annotated
+
+from app.models import table_registry
+from app.core.config.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def init_db():
+    table_registry.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+Db_session = Annotated[Session, Depends(get_db)]
