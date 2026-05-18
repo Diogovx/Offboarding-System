8b5b5bf78bc281575174a00e115fa2fc3dc33304
Lucas Matias <lucasmatias2005@gmail.com>
2026-05-18 14:18:11 -0300

    :sparkles: feat: Adding logic to retrieve IFS user

--- app/integrations/ifs/service.py
+++ app/integrations/ifs/service.py
@@ -1,7 +1,8 @@
 import logging
+import asyncio # to test
 import httpx
 from app.core.config import settings
-from app.integrations.ifs.schemas import IFSTokenResponse, IFSTokenRequest
+from app.integrations.ifs.schemas import IFSTokenResponse, IFSTokenRequest, IFSUserResponse
 
 logger = logging.getLogger(__name__)
 
@@ -10,13 +11,10 @@ class IFSService:
     def __init__(self, base_url: str): 
         self.base_url = base_url.rstrip("/")
         self._token = None
-        self.headers = {
-            "accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded"
-           
-        }   
 
-    async def _get_token(self, client: httpx.AsyncClient) -> str:
+        self.headers = {}   
+
+    async def _get_token_ifs(self, client: httpx.AsyncClient) -> str:
         url = f"{self.base_url}/openid-connect-provider/idp/token"
 
         if self._token:
@@ -29,12 +27,37 @@ class IFSService:
             client_secret=settings.IFS_CLIENT_SECRET
         )
 
-        logger.info("Requesting access token for IFS...")
+        logger.info("Find id_token...")
 
-        response = await client.post(url, data=payload.model_dump(), headers=self.headers)
+        token_headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        response = await client.post(url, data=payload.model_dump(), headers=token_headers)
         response.raise_for_status() 
 
         token_data = IFSTokenResponse(**response.json())
-        self._token = token_data.access_token
+        self._token = token_data.id_token
 
         return self._token
+    
+    async def _get_user_ifs(self, registration: str, client: httpx.AsyncClient) -> IFSUserResponse:
+        
+        url = f"{self.base_url}/main/ifsapplications/projection/v1/UserRelatedData.svc/Reference_FndUser(Identity='{registration}')"
+
+        if not self._token:
+            await self._get_token_ifs(client) 
+
+        logger.info(f"Buscando colaborador no IFS: {registration}")
+
+        search_headers = {
+            "Authorization": f"Bearer {self._token}",
+            "Accept": "application/json"
+        }
+
+        response = await client.get(url, headers=search_headers)
+        response.raise_for_status()
+
+        user_data = IFSUserResponse(**response.json())
+        return user_data
